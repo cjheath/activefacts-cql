f626b31ea3749f76c7a2e7945a9dcb9efa830c51
Clifford Heath <clifford.heath@gmail.com>
2018-11-29 13:16:01 +1100

    Updated heading levels and improved some text

--- js/diagrams/statement.js
+++ js/diagrams/statement.js
@@ -1,14 +1,14 @@
 ComplexDiagram(
-  Choice(0,
-    NonTerminal('subset_constraint', {href: '#subset_constraint'}),
-    NonTerminal('equality_constraint', {href: '#equality_constraint'}),
-    NonTerminal('set_constraint', {href: '#set_constraint'}),
-    NonTerminal('presence_constraint', {href: '#presence_constraint'})
+  Choice(3,
+    NonTerminal('informal_description', {href: '#informal_description'}),
     NonTerminal('unit_definition', {href: '#unit_definition'}),
     NonTerminal('object_type', {href: '#object_type'}),
     NonTerminal('fact_type', {href: '#fact_type'}),
-    NonTerminal('informal_description', {href: '#informal_description'}),
     NonTerminal('query', {href: '#query'}),
+    NonTerminal('subset_constraint', {href: '#subset_constraint'}),
+    NonTerminal('equality_constraint', {href: '#equality_constraint'}),
+    NonTerminal('set_constraint', {href: '#set_constraint'}),
+    NonTerminal('presence_constraint', {href: '#presence_constraint'}),
     /* REVISIT: Define transform_rule */
     NonTerminal('transform_rule', {href: '#transform_rule'}),
     /* REVISIT: Define empty_statement */
